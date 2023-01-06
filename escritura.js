840f2f174628e9baddb6ff21286070624e04e42e
Volkney <jorge.24.co@gmail.com>
2023-01-06 17:21:57 -0600

    Added functions for player and computer's choice

--- escritura.js
+++ escritura.js
@@ -9,6 +9,33 @@ function getComputerChoice() {
 
 console.log(getComputerChoice());
 
-function playerChoice() {
-    prompt = 'What is your option for rock paper scissors'
+function question () {
+    let player = prompt('What is your option for rock paper and scissors');
+    return player;
 }
+
+
+let value = false;
+function playerChoice() {
+    while (value!=true)
+        var player = prompt('What is your option for rock paper and scissors');
+        switch (player) {
+            case 'rock' || 'ROCK' || 'Rock': 
+            value = true
+            break;
+
+            case 'paper' || 'PAPER' || 'Paper': 
+            value = true
+            break;
+
+            case 'Scissors' || 'SCISSORS' || 'Scissors': 
+            value = true
+            break;
+
+            default:
+                console.log('That is not a valid option')
+
+        }
+}
+
+playerChoice();
