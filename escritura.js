72f916e6e26640612fbb8cad3fc80fe0c7cb2b83
Volkney <jorge.24.co@gmail.com>
2023-01-07 16:55:04 -0600

    playRound function added to play one round

--- escritura.js
+++ escritura.js
@@ -1,45 +1,41 @@
-/* get computer choice */
 
+/* variables */
 const options = ["Rock", "Paper", "Scissors"];
+let input = ""; /* variable for players choice */
+let computerChoice;
+let playerChoice;
 
+/* get computer choice */
 function getComputerChoice() {
-        let computerChoice = console.log(options[Math.floor(Math.random() * 3)]);
+        const computerChoice = options[Math.floor(Math.random() * 3)];
+        /* console.log(computerChoice); */
         return computerChoice
 }
+/* get players choice */
 
-console.log(getComputerChoice());
-
-function question () {
-    let player = prompt('What is your option for rock paper and scissors');
-    return player;
-}
-
-
-let input;
-
-function playerChoice(input) {
+function getPlayerChoice() {
     
     while (true) {
         input = prompt('What is your option for rock paper and scissors');
-        let value = input.toString();
-        switch (value) {
+        const playerChoice = input.toString(); /* turn input to string to use in switch case */
+        switch (playerChoice) {
             case "rock":
             case "ROCK":
             case "Rock":
                 console.log("You chose rock");
-                return; 
+                return 'Rock'; 
 
             case "paper":
             case "Paper":
             case "PAPER":
                 console.log("You chose paper");
-                return; 
+                return 'Paper'; 
 
             case "scissors":
             case "Scissors":
             case "SCISSORS":
                 console.log("You chose scissors");
-                return; 
+                return 'Scissors'; 
 
             default:
                 console.log('That is not a valid option')
@@ -48,4 +44,31 @@ function playerChoice(input) {
     }
 }
 
-playerChoice(input);
+
+function playRound(){
+
+    const computerChoice = getComputerChoice();
+    const playerChoice = getPlayerChoice();
+
+    switch (true) {
+        
+        case computerChoice === playerChoice:
+            console.log(`It's a tie! Both players chose ${computerChoice}.`);
+            break;
+        case (playerChoice === 'Paper' && computerChoice == 'Rock') ||
+            (playerChoice === 'Rock' && computerChoice == 'Scissors') ||
+            (playerChoice === 'Scissors' && computerChoice == 'Paper'):
+            console.log(`Player chose ${playerChoice} and the Computer chose ${computerChoice}. You won!`)
+            break;
+        
+            case (playerChoice === 'Rock' && computerChoice == 'Paper') ||
+            (playerChoice === 'Scissors' && computerChoice == 'Rock') ||
+            (playerChoice === 'Paper' && computerChoice == 'Scissors'):
+            console.log(`You lost! Player chose ${playerChoice} and the Computer chose ${computerChoice}`);
+            break;
+
+            
+    }
+
+}
+playRound();
